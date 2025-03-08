a5d467f3ed48940ade867e1359524b42342c779b
Shivam Kushwaha <kushvaha5678@gmail.com>
2025-03-08 20:58:11 +0530

    add files

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { , validateArgs } from 'firebase/data-connect';
+
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'rtple-learningwebsite',
+  location: 'us-central1'
+};
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,9 @@
+const { , validateArgs } = require('firebase/data-connect');
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'rtple-learningwebsite',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
